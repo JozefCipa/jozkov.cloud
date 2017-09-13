45652a27153473c43607d919adc6a1c118839f59
Jozef Cipa <cipa.jozef@gmail.com>
2017-09-13 23:16:40 +0200

    Preparations for launching app on server

--- dependencies.py
+++ dependencies.py
@@ -2,22 +2,36 @@
 
 # check if all external modules are installed
 
+ok = True
 try:
     import flask
 except ImportError:
     print('Install flask')
+    ok = False
 
 try:
     import redis
 except ImportError:
     print('Install redis')
+    ok = False
 
 try:
     import slugify
 except ImportError:
     print('Install unicode-slugify')
+    ok = False
 
 try:
     import validators
 except ImportError:
     print('Install validators')
+    ok = False
+
+try:
+    import uwsgi
+except ImportError:
+    print('Install uwsgi')
+    ok = False
+
+if ok:
+	print('OK, everything is installed.')
